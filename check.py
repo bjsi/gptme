422b8c51a56ea0d75718da6df3d531e5309842a6
Jamesb <experimentallearning0@gmail.com>
2024-12-18 17:09:25 +0000

    Update test_execute_shell_uses_execute_shell_impl to use correct argument format

--- check.py
+++ check.py
@@ -9,5 +9,5 @@ def test_execute_shell_uses_get_shell_command():
 def test_execute_shell_uses_execute_shell_impl():
     with patch('gptme.tools.shell.execute_shell_impl') as mock_execute_shell_impl:
         mock_execute_shell_impl.return_value = iter([])
-        list(execute_shell('echo "test"', None, None, lambda _: True))
+        list(execute_shell(None, None, {"command": 'echo "test"'}, lambda _: True))
         mock_execute_shell_impl.assert_called_once()
